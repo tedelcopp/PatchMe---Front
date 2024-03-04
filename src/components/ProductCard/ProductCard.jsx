94eef6adb161286169464e475d6f2a7aee1bd59e
tedelcopp <tomas.edelcopp@gmail.com>
2024-03-04 16:41:58 -0300

    WIP.Style-Carrusel

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaCartPlus } from "react-icons/fa";
 import { FaHeartCirclePlus } from "react-icons/fa6";
 import styles from "../ProductCard/ProductCard.module.css";
 
 export default function ProductCard() {
+  const [cartShow, setCartShow] = useState(false);
+  const [heartShow, setHeartShow] = useState(false);
   return (
     <div className={styles.card}>
       <img
@@ -12,23 +14,22 @@ export default function ProductCard() {
         width="auto"
         height="auto"
         alt="watch"
-        />
-    <div className={styles.cardBody}>
-  <div className={styles.cvp}>
-    <h5 className={styles.cardTitle}>
-      Patch Wine Lover
-    </h5>
-    <p className={styles.cardPrice}>$3500</p>
-    <div className={styles.cardIcons} >
-      <a className={styles.cardHeart}>
-        <FaHeartCirclePlus />
-      </a>
-      <a className={styles.cardCart}>
-        <FaCartPlus />
-      </a>
-    </div>
-  </div>
-</div>
+      />
+      <div className={styles.cardBody}>
+        <div className={styles.cvp}>
+          <h5 className={styles.cardTitle}>Patch Wine Lover</h5>
+          <p className={styles.cardPrice}>$3500</p>
+          <div className={styles.cardIcons}>
+            <a className={styles.cardHeart}>
+              <FaHeartCirclePlus />
+            </a>
+            <a className={styles.cardCart}>
+              <FaCartPlus />
+            </a>
+          </div>
         </div>
+      </div>
+
+    </div>
   );
 }
