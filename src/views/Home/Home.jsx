419ae949472e03adba49d2e56f7c46a2748adb0f
tedelcopp <tomas.edelcopp@gmail.com>
2024-03-08 16:11:15 -0300

    DesingResponsive.WIP

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import Carrusel from "../../components/CarruselHome/CarruselHome";
 import Offerts from '../../components/CarruselOfferts/CarruselOfferts';
 import Icons from '../../components/Icons/Icons';
+import styles from "../Home/Home.module.css"
 
 export default function Home() {
   return (
-    <div style={{ marginTop: '5px', overflowX: 'hidden' }}>
+    <div  className={styles.backgroundHome}>
       <Carrusel />
       <Icons />
       <div style={{ marginTop: '5px' }}>
