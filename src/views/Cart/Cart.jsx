29062917eda2f72876d9816774447d2ecf2c70bc
tedelcopp <tomas.edelcopp@gmail.com>
2024-05-22 20:13:42 -0300

    Cart.WIP

--- src/views/Cart/Cart.jsx
+++ src/views/Cart/Cart.jsx
@@ -1,31 +1,70 @@
 import React, { useState } from "react";
-import { RiHeartAddFill } from "react-icons/ri";
+import { toast, Toaster } from "react-hot-toast";
+import { FaCartPlus } from "react-icons/fa";
 import { MdDeleteForever } from "react-icons/md";
-import { FaHeartCircleMinus } from "react-icons/fa6";
+import { BsCartDashFill } from "react-icons/bs";
 import styles from "./Cart.module.css";
+import patch1 from "../../assets/Logo/Página 2.jpeg";
 
 export default function Cart() {
   const [products, setProducts] = useState([
-    { id: 1, name: "Patch 1", price: "$10", quantity: 1, stock: 5 },
-    { id: 2, name: "Patch 2", price: "$15", quantity: 1, stock: 10 },
-    { id: 3, name: "Patch 3", price: "$20", quantity: 1, stock: 8 },
+    {
+      id: 1,
+      name: "Patch 1",
+      price: "$10",
+      quantity: 1,
+      stock: 5,
+      img: patch1,
+    },
+    {
+      id: 2,
+      name: "Patch 2",
+      price: "$15",
+      quantity: 1,
+      stock: 10,
+      img: patch1,
+    },
+    {
+      id: 3,
+      name: "Patch 3",
+      price: "$20",
+      quantity: 1,
+      stock: 8,
+      img: patch1,
+    },
   ]);
 
   const clearWishlist = () => {
     setProducts([]);
+    toast("Vaciaste tu carrito.", {
+      icon: "🛒",
+    });
   };
-
   const removeWishlist = (productId) => {
     const updatedProducts = products.filter(
       (product) => product.id !== productId
     );
     setProducts(updatedProducts);
+    toast("Patch eliminado.", {
+      icon: "❌",
+    });
   };
 
   const addToQuantity = (productId) => {
     const updatedProducts = products.map((product) => {
       if (product.id === productId && product.quantity < product.stock) {
+        toast("Sumaste un Patch.", {
+          icon: "➕",
+        });
         return { ...product, quantity: product.quantity + 1 };
+      } else if (
+        product.id === productId &&
+        product.quantity >= product.stock
+      ) {
+        toast.error("¡Stock alcanzado!", {
+          icon: "⛔️",
+        });
+        return product;
       }
       return product;
     });
@@ -35,7 +74,15 @@ export default function Cart() {
   const lessToQuantity = (productId) => {
     const updatedProducts = products.map((product) => {
       if (product.id === productId && product.quantity > 1) {
+        toast("Restaste un Patch.", {
+          icon: "➖",
+        });
         return { ...product, quantity: product.quantity - 1 };
+      } else if (product.id === productId && product.quantity === 1) {
+        toast.error("¡Mínimo 1 Patch!", {
+          icon: "⛔️",
+        });
+        return product;
       }
       return product;
     });
@@ -43,41 +90,68 @@ export default function Cart() {
   };
 
   const moveToCart = () => {
-    console.log("Productos movidos al carrito:", products);
+    console.log("Patchs enviados al carrito", products);
     setProducts([]);
+    toast("Gracias por tu compra!", {
+      icon: "👏",
+    });
   };
 
   return (
-    <div className={styles.mainWishlist}>
-      <h2 className={styles.wishTitle}>
+    <div className={styles.mainCart}>
+      <Toaster position="top-right" reverseOrder={true} />
+      <h2 className={styles.cartTitle}>
         <u>Cart</u>🛒
       </h2>
 
       {products.length === 0 ? (
-        <p className={styles.wishEmpty}>
+        <p className={styles.cartEmpty}>
           No hay patchs cargados en tu carrito de compras.
         </p>
       ) : (
         <div className={styles.productList}>
           {products.map((product) => (
             <div key={product.id} className={styles.productItem}>
-              <span>{product.name}</span> - <span>{product.price}</span> -{" "}
-              <span> Cantidad: {product.quantity}</span> -{" "}
-              <span>
-                <u> Stock</u>: {product.stock}
-              </span>
-              <div>
+              <div className={styles.productInfo}>
+                <img
+                  src={product.img}
+                  alt={product.name}
+                  className={styles.productImage}
+                />
+                <span className={styles.detail}>
+                  <b>{product.name}</b>
+                </span>
+                <span className={styles.detail}>
+                  <b>
+                    <u>Precio:</u>
+                  </b>{" "}
+                  {product.price}
+                </span>
+                <span className={styles.detail}>
+                  <b>
+                    <u>Cantidad:</u>
+                  </b>{" "}
+                  {product.quantity}
+                </span>
+                <span className={styles.detail}>
+                  <b>
+                    <u>Stock:</u>
+                  </b>{" "}
+                  {product.stock}
+                </span>
+              </div>
+              <div className={styles.productButtons}>
                 <button
                   className={styles.addButton}
                   onClick={() => addToQuantity(product.id)}
                 >
-                  <RiHeartAddFill />
+                  <FaCartPlus />
                 </button>
                 <button
                   className={styles.deleteButton}
                   onClick={() => lessToQuantity(product.id)}
                 >
-                  <FaHeartCircleMinus />
+                  <BsCartDashFill />
                 </button>
                 <button
                   className={styles.deleteButton}
@@ -97,7 +171,7 @@ export default function Cart() {
             Vaciar Carrito
           </button>
           <button className={styles.wishCartButton} onClick={moveToCart}>
-            Finalizar Compra
+            Comprar
           </button>
         </div>
       )}
